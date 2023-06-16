a9d1596f1a40b963910b54d3639622047aec258a
Yury Sauko <yury.savko@gmail.com>
2023-06-16 12:09:16 +0300

    The desktop version is complited, some changes in styles, deleted jQuery, all scripts are made in JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,51 @@
 // Initialization WOW.js
 new WOW().init();
 
-// jQuery
-$(document).ready(function(){
+// Menu hidden-visible
+window.addEventListener("scroll", function () {
+    document.querySelector(".menu").classList.toggle("menu_hidden", window.scrollY < 130);
+});
 
-    // Menu hidden-visible
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 300) {
-            $('.menu').fadeIn('slow');
-        } else {
-            $('.menu').fadeOut('slow');
+//Menu item of the active section & smooth scroll
+const getId = (link) => link.getAttribute('href').replace('#', '');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            document.querySelectorAll('.menu__link').forEach((link) => {
+                link.classList.toggle(
+                    'menu__link_active',
+                    getId(link) === entry.target.id
+                );
+            });
         }
     });
+}, {
+    threshold: 0.9,
+});
 
-// End of $(document).ready(function(){
+document.querySelectorAll('.section').forEach(
+    (section) => observer.observe(section),
+);
+
+document.querySelector('.menu__list').addEventListener('click', (event) => {
+    if (event.target.classList.contains('menu__link')) {
+        event.preventDefault();
+
+        window.scrollTo({
+            top: document.getElementById(getId(event.target)).offsetTop,
+            behavior: "smooth",
+        });
+    }
+});
+
+document.querySelector('.main-sect__to-next-sect-arrow').addEventListener('click', (event) => {
+    if (event.target.classList.contains('main-sect__to-next-sect-link')) {
+        event.preventDefault();
+
+        window.scrollTo({
+            top: document.getElementById(getId(event.target)).offsetTop,
+            behavior: "smooth",
+        });
+    }
 });
